f6c55e810ac5c61437a9a8e9d69f212f47329132
Kaiming Liu <114066532+Aya-Chriskey@users.noreply.github.com>
2023-12-14 13:25:41 +0800

    Add files via upload

---
+++ src/views/contact/response.vue
@@ -0,0 +1,103 @@
+<template>
+  <el-table :data="tableData" style="width: 100%" max-height=650 @selection-change="handleSelectionChange">
+    <el-table-column type="selection" width="55" sortable/>
+    <el-table-column fixed prop="date" label="Date" sortable width="150" />
+    <el-table-column prop="name" label="Name" width="120" />
+    <el-table-column prop="state" label="State" width="120" />
+    <el-table-column prop="city" label="City" width="120" />
+    <el-table-column prop="address" label="Address" width="500" />
+    <el-table-column prop="zip" label="Zip" width="120" />
+    <el-table-column fixed="right" label="Operations" width="120">
+      <template #default="scope">
+        <el-button
+          link
+          type="primary"
+          size="small"
+          @click.prevent="deleteRow(scope.$index)"
+        >
+          Remove
+        </el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+  <el-button class="mt-4" style="width: 100%" @click="onAddItem"
+    >Add Item</el-button
+  >
+  
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import dayjs from 'dayjs'
+import { ElTable } from 'element-plus'
+
+const now = new Date()
+
+
+interface User {
+  date: string
+  name: string
+  address: string
+}
+
+const multipleTableRef = ref<InstanceType<typeof ElTable>>()
+const multipleSelection = ref<User[]>([])
+const toggleSelection = (rows?: User[]) => {
+  if (rows) {
+    rows.forEach((row) => {
+      // TODO: improvement typing when refactor table
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      multipleTableRef.value!.toggleRowSelection(row, undefined)
+    })
+  } else {
+    multipleTableRef.value!.clearSelection()
+  }
+}
+const handleSelectionChange = (val: User[]) => {
+  multipleSelection.value = val
+}
+
+const tableData = ref([
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+])
+
+const deleteRow = (index: number) => {
+  tableData.value.splice(index, 1)
+}
+
+const onAddItem = () => {
+  now.setDate(now.getDate() + 1)
+  tableData.value.push({
+    date: dayjs(now).format('YYYY-MM-DD'),
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  })
+}
+</script>
