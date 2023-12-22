552d61664321b1c1eb26b0a9c8fa9937e38d4018
QuadnucYard <2380433991@qq.com>
2023-12-22 21:19:52 +0800

    Cleanup more

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -5,14 +5,8 @@ const routes: RouteRecordRaw[] = [
     path: "/",
     name: "index",
     component: () => import("@/layouts/manage.vue"),
-    meta: { keepalive: false },
+    meta: { requireAuth: true },
     children: [
-      {
-        path: "/main",
-        name: "main",
-        component: () => import("@/views/Main.vue"),
-        meta: { keepalive: false },
-      },
       {
         path: "/reservation",
         component: () => import("@/views/Useview.vue"),
@@ -53,44 +47,26 @@ const routes: RouteRecordRaw[] = [
     ],
   },
   {
-    path: "/:catchAll(.*)*",
-    name: "404",
-    component: () => import("@/views/Error404.vue"),
-  },
-  {
-    path: "/testlnav",
-    name: "testlnav",
-    component: () => import("@/views/testlnav.vue"),
-  },
-  {
-    path: "/try",
-    name: "try",
-    component: () => import("@/views/try.vue"),
-  },
-  {
-    path: "/tabletest",
-    name: "tabletest",
-    component: () => import("@/views/tabulation/tabletest.vue"),
-  },
-  {
     path: "/auth",
     name: "auth",
     component: () => import("@/views/login/LoginPage.vue"),
-    meta: { keepalive: false },
     children: [
       {
         path: "/auth/login",
         name: "login",
         component: () => import("@/views/login/Login.vue"),
-        meta: { keepalive: false },
       },
       {
         path: "/auth/register",
         name: "register",
         component: () => import("@/views/login/Register.vue"),
-        meta: { keepalive: false },
       },
     ],
+  },
+  {
+    path: "/:catchAll(.*)*",
+    name: "404",
+    component: () => import("@/views/Error404.vue"),
   },
 ];
 
