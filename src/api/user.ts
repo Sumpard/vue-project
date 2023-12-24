7e6722aeb2f5a68670462530d01f1ade62d82910
QuadnucYard <2380433991@qq.com>
2023-12-24 22:15:29 +0800

    Remove redundant code

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,4 +1,3 @@
-import { Page, Pagination, castPagination } from "./page";
 import api from "./request";
 import { Result } from "./result";
 
@@ -11,26 +10,12 @@ export interface User {
   avatar: string;
   user_role: "REGULAR_USER" | "MANAGER";
 }
-
-
-export type UserStats = {
-  total: int;
-  register_today: int;
-  login_today: int;
-  active_today: int;
-};
-
 export async function getUserMe() {
   return (await api.post<Result<User>>("/user/select-self")).data.data;
 }
 
 export async function updateUser(user_id: string, user_data: any) {
   return (await api.put(`/users/${user_id}`, user_data)).data;
-}
-
-export async function getUsers(page: Pagination) {
-  const params = castPagination(page);
-  return (await api.get<Page<User>>("/users/", { params })).data;
 }
 
 export async function updateUserMe(user_data: any) {
@@ -41,12 +26,8 @@ export async function updateUserMeAvatar(user_avatar: File) {
   return (await api.postForm<User>("/users/me/avatar", { file: user_avatar })).data;
 }
 
-export async function getUserStats() {
-  return (await api.get<UserStats>("/users/stat")).data;
-}
-
-export async function updatepassword(oldPassword : string, newPassword: string) {
-  return (await api.putForm("/user/update-password", {old_pwd: oldPassword,new_pwd: newPassword })).data;
+export async function updatepassword(oldPassword: string, newPassword: string) {
+  return (await api.putForm("/user/update-password", { old_pwd: oldPassword, new_pwd: newPassword })).data;
 }
 
 export async function updateemail(email: string) {
