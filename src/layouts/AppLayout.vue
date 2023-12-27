fff9f7cdef2f38b0dc9158739b4253897970dc7f
QuadnucYard <2380433991@qq.com>
2023-12-28 00:02:38 +0800

    Organize header layout

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,19 +1,7 @@
 <template>
   <div class="common-layout">
     <el-container>
-      <el-header>
-        <div class="header-content">
-          <div>
-            <img
-              src="http://jxsy.seu.edu.cn/wjx/images/logo-26b1900697d4ddeb7634576944ba5a87.jpg"
-              style="padding: 5px; height: 65px"
-            />
-          </div>
-          <div class="right-side">
-            <app-header />
-          </div>
-        </div>
-      </el-header>
+      <el-header> <app-header /> </el-header>
       <el-container>
         <app-aside />
         <el-main> <router-view /> </el-main>
@@ -25,27 +13,10 @@
 <script setup lang="ts"></script>
 
 <style scoped>
-.el-row {
-  align-items: center;
-  vertical-align: middle;
-  padding-top: 10px;
-}
-
 .el-header {
   height: 75px;
   background-color: #327be4;
   overflow: hidden;
   position: relative;
-}
-
-.header-content {
-  display: flex;
-  justify-content: space-between;
-  height: 100%;
-  align-items: center;
-}
-
-.right-side {
-  margin-right: 40px;
 }
 </style>
