b7d389e5108608c2ea6a1860b8af7014056919ba
QuadnucYard <2380433991@qq.com>
2023-12-31 16:06:54 +0800

    Sort routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -7,91 +7,118 @@ const routes: RouteRecordRaw[] = [
     meta: { requiresAuth: true },
     children: [
       {
-        path: "/reserve/record",
-        component: () => import("@/views/contents/reserve/reserve-record.vue"),
-      },
-      {
-        path: "/reserve/room",
-        component: () => import("@/views/contents/reserve/reserve-room.vue"),
-      },
-      {
-        path: "/reserve/equipment",
-        component: () => import("@/views/contents/reserve/reserve-equipment.vue"),
-      },
-      {
-        path: "/reserve/seat",
-        component: () => import("@/views/contents/reserve/reserve-seat.vue"),
-      },
-      {
-        path: "/homepage",
+        path: "homepage",
         name: "homepage",
         alias: "/",
         component: () => import("@/views/home/index.vue"),
       },
       {
-        path: "/admin/notice/create",
-        component: () => import("@/views/admin/notice/notice-create.vue"),
+        path: "notice",
+        children: [
+          {
+            path: "list",
+            component: () => import("@/views/contents/notice/notice-list.vue"),
+          },
+          {
+            path: "",
+            component: () => import("@/views/contents/notice/notice-detail.vue"),
+          },
+        ],
       },
       {
-        path: "/admin/notice/list",
-        component: () => import("@/views/admin/notice/notice-list.vue"),
-      },
-      {
-        path: "/notice",
-        component: () => import("@/views/contents/notice/notice-detail.vue"),
-      },
-      {
-        path: "/admin/notice/edit",
-        component: () => import("@/views/admin/notice/notice-edit.vue"),
-      },
-      {
-        path: "/notice/list",
-        component: () => import("@/views/contents/notice/notice-list.vue"),
-      },
-      {
-        path: "/user/home",
+        path: "user/home",
         name: "user-home",
         component: () => import("@/views/user/index.vue"),
       },
       {
-        path: "/feedback/submit",
-        component: () => import("@/views/contents/feedback/feedback-submit.vue"),
+        path: "reserve",
+        children: [
+          {
+            path: "room",
+            component: () => import("@/views/contents/reserve/reserve-room.vue"),
+          },
+          {
+            path: "seat",
+            component: () => import("@/views/contents/reserve/reserve-seat.vue"),
+          },
+          {
+            path: "equipment",
+            component: () => import("@/views/contents/reserve/reserve-equipment.vue"),
+          },
+          {
+            path: "record",
+            component: () => import("@/views/contents/reserve/reserve-record.vue"),
+          },
+        ],
       },
       {
-        path: "/feedback/list",
-        component: () => import("@/views/contents/feedback/feedback-list.vue"),
+        path: "feedback",
+        children: [
+          {
+            path: "submit",
+            component: () => import("@/views/contents/feedback/feedback-submit.vue"),
+          },
+          {
+            path: "list",
+            component: () => import("@/views/contents/feedback/feedback-list.vue"),
+          },
+        ],
       },
       {
-        path: "/admin/entities",
-        component: () => import("@/views/admin/entity/entity-list.vue"),
-        meta: { requiresAdmin: true },
-      },
-      {
-        path: "/admin/users",
-        component: () => import("@/views/admin/user/user-list.vue"),
-        meta: { requiresAdmin: true },
-      },
-      {
-        path: "/upload-image",
-        component: () => import("@/views/fun/UploadImage.vue"),
-      },
-      {
-        path: "/admin/feedback/list",
-        component: () => import("@/views/admin/feedback/feedback-list.vue"),
-      },
-      {
-        path: "/admin/review/submitted",
-        component: () => import("@/views/admin/review/review-submitted.vue"),
-        meta: { requiresAdmin: true },
-      },
-      {
-        path: "/admin/review/accepted",
-        component: () => import("@/views/admin/review/review-accepted.vue"),
-        meta: { requiresAdmin: true },
-      },
-      {
-        path: "/admin/review/refused",
-        component: () => import("@/views/admin/review/review-refused.vue"),
+        path: "admin",
+        children: [
+          {
+            path: "users",
+            component: () => import("@/views/admin/user/user-list.vue"),
+          },
+          {
+            path: "entities",
+            component: () => import("@/views/admin/entity/entity-list.vue"),
+          },
+          {
+            path: "notice",
+            children: [
+              {
+                path: "create",
+                component: () => import("@/views/admin/notice/notice-create.vue"),
+              },
+              {
+                path: "edit",
+                component: () => import("@/views/admin/notice/notice-edit.vue"),
+              },
+              {
+                path: "list",
+                component: () => import("@/views/admin/notice/notice-list.vue"),
+              },
+            ],
+          },
+          {
+            path: "review",
+            children: [
+              {
+                path: "submitted",
+                component: () => import("@/views/admin/review/review-submitted.vue"),
+              },
+              {
+                path: "accepted",
+                component: () => import("@/views/admin/review/review-accepted.vue"),
+              },
+              {
+                path: "refused",
+                component: () => import("@/views/admin/review/review-refused.vue"),
+              },
+            ],
+          },
+          {
+            path: "feedback",
+            children: [
+              {
+                path: "list",
+                component: () => import("@/views/admin/feedback/feedback-list.vue"),
+              },
+            ],
+          },
+        ],
         meta: { requiresAdmin: true },
       },
     ],
